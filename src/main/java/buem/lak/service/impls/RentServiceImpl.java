426bafcb7448696f586b7e5fc63805aa9ea340ac
Vova <vv122333221vv@gmail.com>
2022-08-26 10:52:36 +0200

    Fix. Update services and styles.

--- src/main/java/buem/lak/service/impls/RentServiceImpl.java
+++ src/main/java/buem/lak/service/impls/RentServiceImpl.java
@@ -1,5 +1,6 @@
 package buem.lak.service.impls;
 
+import buem.lak.model.Client;
 import buem.lak.model.Rent;
 import buem.lak.repository.IPaymentMongoRepository;
 import buem.lak.repository.IRentMongoRepository;
@@ -7,10 +8,29 @@ import buem.lak.service.interfaces.IRentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RentServiceImpl implements IRentService {
+
+    private LocalDateTime now = LocalDateTime.now();
+    private List<Rent> rents = new ArrayList<>(
+            Arrays.asList(
+                    new Rent(UUID.randomUUID().toString(), "payment1", "By car", now, now, now, now),
+                    new Rent(UUID.randomUUID().toString(), "payment2", "By car", now, now, now, now),
+                    new Rent(UUID.randomUUID().toString(), "payment3", "By car", now, now, now, now)
+            ));
+
+//    @PostConstruct
+//    void init(){
+//        repository.saveAll(rents);
+//    }
+
     @Autowired
     private IRentMongoRepository repository;
 
